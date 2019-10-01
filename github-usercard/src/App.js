5995cf298afa51c5f3119b93e23610713c0039c7
Sandrava Philips <lacasandra1@gmail.com>
2019-10-01 14:43:59 +0100

    added data to state and built my componentdidmount method

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      users: [
+        { imageUrl: '',
+          username: '',
+          followersCount: 0,
+          followingCount: 0,
+          githubAddress: ''
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/Sandravaphilips/followers')
+    .then(response => {
+      const dataFromApi = response.data;
+      this.setState(currentState => {
+        return {
+          users: {...currentState, dataFromApi}
+        }
+      })
+    })
+  }
+  render() {
+    return (
+      
+    )
+  }
 }
 
 export default App;
